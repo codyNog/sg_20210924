a1c8a4ceda998b2b2687eb1cfbaf489209fe6559
codyNog <yoneyoneyone99@gmail.com>
2021-09-24 15:51:25 +0900

    UserItemのUI修正

--- src/components/molecules/UserItem/index.tsx
+++ src/components/molecules/UserItem/index.tsx
@@ -1,14 +1,19 @@
 import { User } from "~/graphql/types";
+import styled from "@emotion/styled";
 
 interface Props {
   user: User;
 }
 
+const Wrapper = styled.div({
+  border: "1px solid black"
+});
+
 export const UserItem: React.FC<Props> = ({ user }) => {
   return (
-    <div>
+    <Wrapper>
       <div>{user.name}</div>
       <div>{user.age}</div>
-    </div>
+    </Wrapper>
   );
 };
